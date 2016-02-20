39837fb622b8b590613b7f0de2b82ae40c90957e
kne <sirkne@gmail.com>
2016-02-20 08:33:33 -0500

    more casting internally, even if ugly

--- pybox2d/shape.pyx
+++ pybox2d/shape.pyx
@@ -7,6 +7,10 @@ cdef class Shape:
     cdef b2Shape *shape
     cdef bool owner
 
+    def __cinit__(self):
+        self.shape = NULL
+        self.owner = False
+
     @staticmethod
     cdef upcast(const b2Shape *shape):
         # TODO copies for now
@@ -24,22 +28,18 @@ cdef class Shape:
 
 
 cdef class CircleShape(Shape):
-    cdef b2CircleShape *circle
-
     def __cinit__(self):
-        self.circle = new b2CircleShape()
-        self.shape = self.circle
+        self.shape = <b2Shape*>new b2CircleShape()
         self.owner = True
 
     def __dealloc__(self):
         if self.owner:
-            del self.circle
+            del self.shape
 
     cdef from_existing(self, b2CircleShape *shape, owner=True):
         if self.owner:
-            del self.circle
+            del self.shape
 
-        self.circle = shape
         self.shape = shape
         self.owner = owner
 
@@ -47,24 +47,25 @@ cdef class CircleShape(Shape):
         if center is None:
             center = (0.0, 0.0)
 
-        self.circle.m_radius = radius
+        (<b2CircleShape *>self.shape).m_radius = radius
         self.center = center
 
     property radius:
         def __get__(self):
-            return self.circle.m_radius
+            return (<b2CircleShape *>self.shape).m_radius
 
         def __set__(self, value):
-            self.circle.m_radius = value
+            (<b2CircleShape *>self.shape).m_radius = value
 
     property center:
         def __get__(self):
-            return Vec2(self.circle.m_p.x, self.circle.m_p.y)
+            return Vec2((<b2CircleShape *>self.shape).m_p.x,
+                        (<b2CircleShape *>self.shape).m_p.y)
 
         def __set__(self, center):
             cx, cy = center
-            self.circle.m_p.x = cx
-            self.circle.m_p.y = cy
+            (<b2CircleShape *>self.shape).m_p.x = cx
+            (<b2CircleShape *>self.shape).m_p.y = cy
 
     def __str__(self):
         return ('{0.__class__.__name__}(radius={0.radius}, center={0.center})'
