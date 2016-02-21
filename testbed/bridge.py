4a2c44243d8488e32ca31c61d181d1d728c7897a
kne <sirkne@gmail.com>
2016-02-20 20:39:39 -0500

    add simple bridge testbed example

---
+++ testbed/bridge.py
@@ -0,0 +1,84 @@
+from __future__ import division
+from pybox2d import (CircleShape, EdgeShape, FixtureDef, PolygonShape)
+
+
+def create_bridge(world, ground, size, offset, plank_count, friction=0.6,
+                  density=1.0):
+    """
+    Create a bridge with plank_count planks,
+    utilizing rectangular planks of size (width, height).
+    The bridge should start at x_offset, and continue to
+    roughly x_offset+width*plank_count.
+    The y will not change.
+    """
+    width, height = size
+    x_offset, y_offset = offset
+    half_height = height / 2
+    plank = FixtureDef(shape=PolygonShape(box=(width / 2, half_height)),
+                       friction=friction,
+                       density=density,
+                       )
+
+    bodies = []
+    prev_body = ground
+    for i in range(plank_count):
+        body = world.create_dynamic_body(
+            position=(x_offset + width * i, y_offset),
+            fixtures=plank,
+        )
+        bodies.append(body)
+
+        world.create_revolute_joint(
+            bodies=(prev_body, body),
+            anchor=(x_offset + width * (i - 0.5), y_offset)
+        )
+
+        prev_body = body
+
+    world.create_revolute_joint(
+        bodies=(prev_body, ground),
+        anchor=(x_offset + width * (plank_count - 0.5), y_offset),
+    )
+    return bodies
+
+
+def setup(world):
+    '''bridge test'''
+    ground_shape = EdgeShape(vertices=[(-40, 0), (40, 0)])
+    ground = world.create_static_body(
+        fixtures=[FixtureDef(shape=ground_shape)],
+    )
+
+    bodies = create_bridge(world, ground=ground,
+                           size=(1.0, 0.25),
+                           offset=(-14.5, 5),
+                           plank_count=30,
+                           friction=0.2, density=20)
+
+    fixture = FixtureDef(
+        shape=PolygonShape(vertices=[(-0.5, 0.0),
+                                     (0.5, 0.0),
+                                     (0.0, 1.5),
+                                     ]),
+        density=1.0
+    )
+
+    for i in range(2):
+        world.create_dynamic_body(
+            position=(-8 + 8 * i, 12),
+            fixtures=fixture,
+        )
+
+    fixture = FixtureDef(shape=CircleShape(radius=0.5), density=1)
+    for i in range(3):
+        world.create_dynamic_body(
+            position=(-6 + 6 * i, 10),
+            fixtures=fixture,
+        )
+
+    return {'bridge_bodies': bodies}
+
+
+if __name__ == '__main__':
+    from main_simple import main
+    main(setup)
---
+++ testbed/main_simple.py
@@ -0,0 +1,149 @@
+try:
+    import pygame_sdl2
+except ImportError:
+    pass
+else:
+    print('Using pygame_sdl2')
+    pygame_sdl2.import_as_pygame()
+
+
+import pygame
+from pygame.locals import (QUIT, KEYDOWN, K_ESCAPE)
+from pybox2d import (World, CircleShape, PolygonShape)
+from pybox2d import (FixtureDef, EdgeShape)
+
+# Box2D deals with meters, but we want to display pixels,
+# so define a conversion factor:
+PPM = 20.0  # pixels per meter
+
+colors = {
+    0: (255, 255, 255, 255),  # static
+    1: (127, 127, 127, 255),  # kinematic
+    2: (127, 100, 100, 255),  # dynamic
+}
+
+def fix_vertices(screen, vertices):
+    screen_width, screen_height = screen.get_width(), screen.get_height()
+    half_width = screen_width / 2.
+    # convert from meters to pixels
+    # flip in y
+    # move over a bit in x
+    return [(int(v0 * PPM + half_width),
+             int(screen_height - v1 * PPM))
+            for v0, v1 in vertices]
+
+
+def pygame_draw_edge(screen, edge, body, fixture):
+    vertices = [body.transform * v
+                for v in edge.main_vertices]
+    v0, v1 = fix_vertices(screen, vertices)
+    pygame.draw.line(screen, colors[body.type], v0, v1)
+
+
+def pygame_draw_polygon(screen, polygon, body, fixture):
+    vertices = [body.transform * v
+                for v in polygon.vertices]
+    vertices = fix_vertices(screen, vertices)
+    pygame.draw.polygon(screen, colors[body.type], vertices)
+
+
+def pygame_draw_circle(screen, circle, body, fixture):
+    center = fix_vertices(screen, [body.transform * circle.center])[0]
+    pygame.draw.circle(screen, colors[body.type], center,
+                       int(circle.radius * PPM),
+                       width=1)
+
+
+def setup_backend(screen_width=640, screen_height=480):
+    screen = pygame.display.set_mode((screen_width, screen_height), 0, 32)
+
+    draw_registry = {}
+    draw_registry[CircleShape] = pygame_draw_circle
+    draw_registry[PolygonShape] = pygame_draw_polygon
+    draw_registry[EdgeShape] = pygame_draw_edge
+    return screen, draw_registry
+
+
+def main_loop(screen, world, draw_registry,
+              world_info, target_fps=60.0):
+
+    if 'title' in world_info:
+        pygame.display.set_caption(world_info['title'])
+
+    running = True
+    clock = pygame.time.Clock()
+    time_step = 1.0 / target_fps
+
+    while running:
+        # Check the event queue
+        for event in pygame.event.get():
+            escape_pressed = (event.type == KEYDOWN and event.key == K_ESCAPE)
+            if event.type == QUIT or escape_pressed:
+                # The user closed the window or pressed escape
+                running = False
+
+        screen.fill((0, 0, 0, 0))
+
+        # Draw the world
+        for body in world.bodies:
+            for fixture in body.fixtures:
+                shape = fixture.shape
+                try:
+                    draw_fcn = draw_registry[type(fixture.shape)]
+                except KeyError:
+                    # this backend can't draw the shape - draw it as a circle
+                    # for now
+                    draw_fcn = pygame_draw_circle
+
+                draw_fcn(screen, shape, body, fixture)
+
+        # Make Box2D simulate the physics of our world for one step.
+        world.step(time_step, 10, 10)
+
+        # Flip the screen and try to keep at the target FPS
+        pygame.display.flip()
+        clock.tick(target_fps)
+
+    pygame.quit()
+
+
+def main(setup_function, target_fps=60.0):
+    world = World(gravity=(0, -10))
+    world_info = setup_function(world)
+
+    if world_info is None:
+        world_info = {}
+
+    if 'title' not in world_info and setup_function.__doc__:
+        world_info['title'] = setup_function.__doc__
+
+    screen, draw_registry = setup_backend()
+    print(screen)
+    return main_loop(screen=screen, world=world, draw_registry=draw_registry,
+                     world_info=world_info, target_fps=target_fps)
+
+
+def simple_setup(world):
+    '''no demo selected'''
+    ground_shape = EdgeShape(vertices=[(-40, 5), (40, 5)])
+    world.create_static_body(
+        fixtures=[FixtureDef(shape=ground_shape)],
+    )
+
+    fixture = FixtureDef(
+        shape=PolygonShape(vertices=[(-0.5, 10.0),
+                                     (0.5, 10.0),
+                                     (0.0, 11.5),
+                                     ]),
+        density=1.0
+    )
+
+    for i in range(2):
+        world.create_dynamic_body(
+            position=(-8 + 8 * i, 12),
+            fixtures=fixture,
+        )
+
+
+if __name__ == '__main__':
+    main(simple_setup)
