6e8acdf9ded380e56c4b020f482152d96b1afe3d
kne <sirkne@gmail.com>
2016-03-19 08:59:55 -0400

    minor doc tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -53,7 +53,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pybox2d'
-copyright = '2016, kne'
+# copyright = '2016, kne'
 author = 'kne'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -181,7 +181,7 @@ html_static_path = ['_static']
 #html_show_sphinx = True
 
 # If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
